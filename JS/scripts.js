f1ad9d64de8fdd2cb90194283007a2243742860d
Sergei Lamkov <lamkov@gmail.com>
2016-10-02 23:23:23 +0300

    final

--- JS/scripts.js
+++ JS/scripts.js
@@ -5,15 +5,23 @@ var usersArray =[
     {age:45, name:"grigorij", height:175}
 ];
 
-function drawTable (){
-var table = "<table class='table'>";
+function drawTable (){	
+	var table = "<table class='table table-striped'>";
+	table += "<thead>";
+	table += "<tr>";
+	table +="<th></th>";
+	table +="<th><a onclick='sortByAge(event)' href='#'>Возраст</a></th>";
+	table +="<th><a onclick='sortByName(event)' href='#'>Имя</a></th>";
+	table +="<th><a onclick='sortByHeight(event)' href='#'>Рост</a></th>";
+	table +="</tr>";
+	table +="</thead>";
+
 for (var i = 0; i < usersArray.length; i++) {
     table +="<tr>";
     table +="<td><input name='checker' class='checker' type='checkbox'/></td>";
     table +="<td>"+usersArray[i]["age"]+"</td>";
     table +="<td>"+usersArray[i]["name"]+"</td>";
     table +="<td>"+usersArray[i]["height"]+"</td>";
-
     table +="</tr>";
 
 }
@@ -35,7 +43,7 @@ function reset() {
     document.getElementById("name").value = "";
     document.getElementById("height").value = "";
 }
-function myFunction() {
+function validateForm() {
     var x, y, z, text;
     x = document.getElementById("age").value;
     y = document.getElementById("name").value;
@@ -52,28 +60,80 @@ function myFunction() {
         text = "Input not valid";
         reset();
     }
-    document.getElementById("demo").innerHTML = text;
-}
-function Remove(index){
-    usersArray.splice(index, index+1);
+    document.getElementById("validationMessage").innerHTML = text;
 }
 
 function deleteSelectedRows() {
     var inputs = document.getElementsByClassName("checker");
-    for (i=0; i<inputs.length; i++) {
+		
+    for (var i = usersArray.length - 1; i >= 0; i--) {
         if(inputs[i].checked) {
-            Remove(i);
+            usersArray.splice(i, 1);
         }
     }
+	
     drawTable();
 }
 
 function copySelectedRows(){
     var inputs = document.getElementsByClassName("checker");
-    for (i=0; i<inputs.length; i++) {
+	
+	for (var i = usersArray.length - 1; i >= 0; i--) {
         if(inputs[i].checked) {
             usersArray.splice(i, 0, usersArray[i]);
         }
     }
+	
+    /*for (i=0; i<inputs.length; i++) {
+        if(inputs[i].checked) {
+            usersArray.push(usersArray[index]);
+        }
+    }*/
     drawTable();
 }
+
+function sortByAge(event) {
+	event.preventDefault();	
+	usersArray.sort(function(a, b) {
+		return (a.age - b.age);
+	});
+	//usersArray.reverse();	
+	drawTable();
+}
+
+function compareObjectsByName (a, b) {
+  if (a.name > b.name) return 1;
+  if (a.name < b.name) return -1;
+  return 0;
+}
+
+function sortByName(event) {
+	var usersArrayClone = [];
+	
+	event.preventDefault();
+	
+	for (var i in usersArray) {
+	  usersArrayClone.push(usersArray[i]);
+	}
+	
+	usersArrayClone.sort(compareObjectsByName);
+	usersArray = usersArrayClone;
+	drawTable();
+}
+
+function sortByHeight(event) {
+	event.preventDefault();	
+	usersArray.sort(function(a, b) {
+		return (a.height - b.height);
+	});
+	drawTable();
+}
+
+
+
+
+
+
+
+
+
